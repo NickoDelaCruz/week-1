d54c8057f31d83842287070dec03abbe6a2ee625
Nicko Dela Cruz <nickodc96@gamil.com>
2018-02-15 20:17:05 -0800

    changed .split

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,13 +3,9 @@ class Anagram
     @word = word1
     @word2 = word2
   end
-
-
-
-
   def checker(input1, input2)
-  @word = input1.downcase.gsub(/[^a-z0-9\s]/i, '').chars.sort
-  @word2 = input2.downcase.gsub(/[^a-z0-9\s]/i, '').chars.sort
+  @word = input1.downcase.gsub(/[^a-z0-9\s]/i, '').split.sort
+  @word2 = input2.downcase.gsub(/[^a-z0-9\s]/i, '').split.sort
     if (input1 && input2 =~ /[aeiou]/)
       return true
     elsif (input1.reverse == input1)
@@ -20,11 +16,8 @@ class Anagram
       return true
     elsif (input1 == input2)
       return true
-
     else
       return "you got a problem buddy"
-
-
     end
   end
 end
